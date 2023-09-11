8b7cea4fba09d0d287c6f7dbf162b0ebd664353f
Juan David <41099211+juandape@users.noreply.github.com>
2023-09-11 14:52:55 -0500

    feat: comoponente pedidos (#1)

---
+++ src/components/Pedidos.js
@@ -0,0 +1,65 @@
+function Pedidos() {
+  return (
+    <div>
+      <h1>Pedidos</h1>
+      <form>
+        <label>
+          Numero Pedido:
+          <input type='number' name='numeroPedido' />
+        </label>
+        <label>
+          linea:
+          <input type='text' name='linea' />
+        </label>
+        <label>
+          Cliente:
+          <input type='text' name='cliente' />
+        </label>
+        <label>
+          Codigo Producto:
+          <input type='text' name='codigoProducto' />
+        </label>
+        <label>
+          Cantidad:
+          <input type='number' name='cantidad' />
+        </label>
+        <label>
+          Valor Unitario:
+          <input type='number' name='valorUnitario' />
+        </label>
+        <label>
+          Valor Transporte:
+          <input type='number' name='valorTransporte' />
+        </label>
+        <label>
+          Valor Total:
+          <input type='number' name='valorTotal' />
+        </label>
+        <label>
+          Fecha Solicitud:
+          <input type='date' name='fechaSolicitud' />
+        </label>
+        <label>
+          Fecha Entrega Estimada:
+          <input type='date' name='fechaEntregaEstimada' />
+        </label>
+        <label>
+          Fecha Entrega Real:
+          <input type='date' name='fechaEntregaReal' />
+        </label>
+        <label>
+          Estado:
+          <select name='estado'>
+            <option value='' disabled selected hidden>Seleccione una opción</option>
+            <option value='pendiente'>Pendiente</option>
+            <option value='asignado'>Inventario asignado</option>
+            <option value='pendientedecobro'>Despachado pendiente de cobro</option>
+            <option value='pagado'>Despachado y cancelado</option>
+          </select>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default Pedidos;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,8 +6,27 @@ import styles from '@/styles/Dashboard.module.css';
 const Dashboard = () => {
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1 className={styles.dashboard__title}>In Kontrol Dashboard</h1>
       <div className={styles.dashboard__container}>
+        <div>
+          <h2>Clientes</h2>
+          <ul>
+            <Link href={'FormularioClientes'}>
+              <li>Crear nuevo Cliente</li>
+            </Link>
+            <li>Total Clientes</li>
+            <li>cant</li>
+            <li>Pendiente entrega</li>
+            <li>$, cant clientes</li>
+            <li>Pendiente Fabricacion</li>
+            <li>$, cant clientes</li>
+            <li>Entregado y Pendiente cobro</li>
+            <li>$, cant clientes</li>
+            <li>Entregado y Cobrado</li>
+            <li>$, cant clientes</li>
+            <li>Recaudo por mes</li>
+          </ul>
+        </div>
         <div>
           <h2>Proveedores</h2>
           <div>Ver listado</div>
@@ -34,26 +53,6 @@ const Dashboard = () => {
             <li>costo</li>
             <li>PT</li>
             <li>valor</li>
-          </ul>
-        </div>
-        <div>
-          <h2>Clientes</h2>
-          <ul>
-            <Link href={'FormularioClientes'}>
-              <li>Crear nuevo Cliente</li>
-            </Link>
-
-            <li>Total Clientes</li>
-            <li>cant</li>
-            <li>Pendiente entrega</li>
-            <li>$, cant clientes</li>
-            <li>Pendiente Fabricacion</li>
-            <li>$, cant clientes</li>
-            <li>Entregado y Pendiente cobro</li>
-            <li>$, cant clientes</li>
-            <li>Entregado y Cobrado</li>
-            <li>$, cant clientes</li>
-            <li>Recaudo por mes</li>
           </ul>
         </div>
       </div>

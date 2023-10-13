d0a80e20030e15c747966e461b477fe661451029
Juan David Peña <juandavidp76@hotmail.com>
2023-10-13 13:50:53 -0500

    clients form styles

--- src/components/ClientsForm.js
+++ src/components/ClientsForm.js
@@ -46,95 +46,94 @@ function ClientsForm() {
   };
 
   return (
-    <div>
-      <a href='/' className={styles.back}>Inicio</a>
+    <div className={styles.container}>
+      <a href='/' className={styles.back}>
+        ← 
+      </a>
       <h1 className={styles.ClientesForm__title}>Formulario Clientes</h1>
       <form className={styles.ClientesForm__container} onSubmit={handleSubmit}>
-        <label>
+        <label className={styles.ClientsForm_container_label}>
           Nombre Cliente:
-          <input
-            type='text'
-            name='name'
-            value={clientes.name}
-            className={styles.ClientesForm__container__input}
-            placeholder='ingresa el nombre'
-            onChange={handleChange}
-            required
-          />
         </label>
-        <label>
-          Email:
-          <input
-            type='email'
-            name='email'
-            value={clientes.email}
-            placeholder='correo@mail.com'
-            pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
-            className={styles.ClientesForm__container__input}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Teléfono:
-          <input
-            type='number'
-            name='phone'
-            value={clientes.phone}
-            placeholder='celular'
-            className={styles.ClientesForm__container__input}
-            onChange={handleChange}
-            maxLength={10}
-            required
-          />
-        </label>
-        <label>
-          Dirección:
-          <input
-            type='text'
-            name='address'
-            value={clientes.address}
-            placeholder='calle 123 # 123-123'
-            className={styles.ClientesForm__container__input}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Barrio:
-          <input
-            type='text'
-            name='neighborhood'
-            value={clientes.neighborhood}
-            placeholder='barrio'
-            className={styles.ClientesForm__container__input}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Ciudad:
-          <input
-            type='text'
-            name='city'
-            value={clientes.city}
-            placeholder='ingresa la ciudad'
-            className={styles.ClientesForm__container__input}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Instagram:
-          <input
-            type='text'
-            name='instagram'
-            value={clientes.instagram}
-            placeholder='@instagram'
-            className={styles.ClientesForm__container__input}
-            onChange={handleChange}
-          />
-        </label>
-        <button type='submit' className={styles.back}>Enviar</button>
+        <input
+          type='text'
+          name='name'
+          value={clientes.name}
+          className={styles.ClientesForm__container__input}
+          placeholder='ingresa el nombre'
+          onChange={handleChange}
+          required
+        />
+
+        <label className={styles.ClientsForm_container_label}>Email:</label>
+        <input
+          type='email'
+          name='email'
+          value={clientes.email}
+          placeholder='correo@mail.com'
+          pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+          className={styles.ClientesForm__container__input}
+          onChange={handleChange}
+        />
+
+        <label className={styles.ClientsForm_container_label}>Teléfono:</label>
+        <input
+          type='number'
+          name='phone'
+          value={clientes.phone}
+          placeholder='celular'
+          className={styles.ClientesForm__container__input}
+          onChange={handleChange}
+          maxLength={10}
+          required
+        />
+
+        <label className={styles.ClientsForm_container_label}>Dirección:</label>
+        <input
+          type='text'
+          name='address'
+          value={clientes.address}
+          placeholder='calle 123 # 123-123'
+          className={styles.ClientesForm__container__input}
+          onChange={handleChange}
+          required
+        />
+
+        <label className={styles.ClientsForm_container_label}>Barrio:</label>
+        <input
+          type='text'
+          name='neighborhood'
+          value={clientes.neighborhood}
+          placeholder='barrio'
+          className={styles.ClientesForm__container__input}
+          onChange={handleChange}
+          required
+        />
+
+        <label className={styles.ClientsForm_container_label}>Ciudad:</label>
+        <input
+          type='text'
+          name='city'
+          value={clientes.city}
+          placeholder='ingresa la ciudad'
+          className={styles.ClientesForm__container__input}
+          onChange={handleChange}
+          required
+        />
+
+        <label className={styles.ClientsForm_container_label}>Instagram:</label>
+        <input
+          type='text'
+          name='instagram'
+          value={clientes.instagram}
+          placeholder='@instagram'
+          className={styles.ClientesForm__container__input}
+          onChange={handleChange}
+        />
+
+        <button type='submit' className={styles.button}>
+          Enviar
+        </button>
       </form>
     </div>
   );

febbbec934eaa0ffb540a724122ad9dc7c2f6c14
Juan David Peña <juandavidp76@hotmail.com>
2023-10-18 21:24:41 -0500

    fix

--- src/components/StatusSelect.jsx
+++ src/components/StatusSelect.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import styles from '@/styles/StatusSelect.module.css';
 
-function StatusSelect({ value, onChange}) {
-  const options = ['Pendiente', 'Inventario asignado', 'Despachado y pendiente de cobro', 'Despachado y cancelado'];
+function StatusSelect({ value, onChange }) {
+  const options = [
+    'Pendiente',
+    'Inventario asignado',
+    'Despachado y pendiente de cobro',
+    'Despachado y cancelado',
+  ];
 
   return (
-    <select value={value} onChange={onChange}>
-      {options.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+    <>
+      <label className={styles.status__title}>Estado</label>
+      <select
+        value={value}
+        onChange={onChange}
+        className={styles.status__options}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </>
   );
 }
 

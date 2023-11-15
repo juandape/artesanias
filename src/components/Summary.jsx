1824504d09261f3374250121b432d4c24c8a460a
Juan David Peña <juandavidp76@hotmail.com>
2023-11-15 15:48:37 -0500

    title

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -29,7 +29,7 @@ export default function Summary() {
 
   return (
     <>
-      <h1>PAGE ON CONSTRUCTION</h1>
+      <h1>PAGE UNDER CONSTRUCTION</h1>
       <div className={styles.container}>
         <a href='/' className={styles.back}>
           ←
